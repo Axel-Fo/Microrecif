8a186b093749bfef77c4bdb62bc1638e57110764
Nestor Guibentif <nestor.guibentif@epfl.ch>
2024-03-17 12:40:19 +0100

    includes +enum

---
+++ simulation.h
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+#include <fstream>//pour ouvrir et lire fichiers
+#include <sstream> 
+// sstream permet de gérer les strings comme flux de donées
+//pour pouvoir les manipuler,changer leur types, effacer 
+//en extraire des trucs etc (comme ca que je l'ai compris)
+
+enum Etat_lecture
+{
+    NBA, ALGUE ,
+    NBC, CORAIL, 
+    NBS, SCAVENGER 
+}
