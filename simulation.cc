ad53839c0b06e0018c6cd6b7f850d1fa8aecb3f4
Nestor Guibentif <nestor.guibentif@epfl.ch>
2024-03-17 12:41:17 +0100

    compile surement pas encore

--- simulation.cc
+++ simulation.cc
@@ -1,1 +1,29 @@
 #include "simulation.h"
+
+
+//on traite le fichier ligne par ligne
+void lecture( char * fichier_entree)
+{
+    string ligne;
+    //la ligne du dessous appe
+    ifstream fichier(fichier_entree); 
+    if(!fichier.fail()) 
+    {
+        
+        while(getline(fichier >> ws,ligne)) 
+        //ws =ignorer espaces blancs
+		{	
+			if(line[0]=='#')  continue;  
+       
+			decodage_ligne(ligne);
+        }
+        cout << "fin de la lecture" << endl;
+	}
+	else cout <<"erreur lgn 22 simu"<<endl;
+}
+
+void decodage_ligne(string ligne)
+{
+
+}
+
